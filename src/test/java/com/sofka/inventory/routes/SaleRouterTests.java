08091491950cac59ba810f216fbe0a6424777e24
Luis Diego Flores Villalobos <flores.luisdiego@gmail.com>
2023-12-20 13:19:26 -0600

    Agregado test para ruta de venta al por mayor
    Agregada prueba unitaria para ruta de venta al por mayor

--- src/test/java/com/sofka/inventory/routes/SaleRouterTests.java
+++ src/test/java/com/sofka/inventory/routes/SaleRouterTests.java
@@ -5,6 +5,7 @@ import com.sofka.inventory.models.dto.InventoryDTO;
 import com.sofka.inventory.models.dto.ProductDTO;
 import com.sofka.inventory.models.mongo.Product;
 import com.sofka.inventory.useCases.SaleAddRetailUseCase;
+import com.sofka.inventory.useCases.SaleAddWholesaleUseCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,12 +25,14 @@ public class SaleRouterTests {
     private WebTestClient webTestClient;
     @Mock
     private SaleAddRetailUseCase saleAddRetailUseCase;
+    @Mock
+    private SaleAddWholesaleUseCase saleAddWholesaleUseCase;
     private SaleHandler saleHandler;
     private SaleRouter saleRouter;
 
     @BeforeEach
     public void setup() {
-        saleHandler = new SaleHandler(saleAddRetailUseCase);
+        saleHandler = new SaleHandler(saleAddRetailUseCase, saleAddWholesaleUseCase);
         saleRouter = new SaleRouter(saleHandler);
         webTestClient = WebTestClient.bindToRouterFunction(saleRouter.saleRoutes())
             .build();
@@ -61,6 +64,40 @@ public class SaleRouterTests {
 
         webTestClient.post()
             .uri("/sale/retail")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(data, InventoryDTO.class)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(ProductDTO.class)
+            .isEqualTo(List.of(resultProduct));
+    }
+
+    @Test
+    void SaleAddWholesaleRouteTest() {
+        ProductDTO productDTO = new ProductDTO();
+        String id = "<product-id>";
+        productDTO.setId(id);
+        int quantityBefore = 100;
+        productDTO.setQuantity(quantityBefore);
+        productDTO.setEnabled(true);
+
+        InventoryDTO inventoryDTO = new InventoryDTO();
+        inventoryDTO.setProduct(productDTO);
+        int saleQuantity = 50;
+        inventoryDTO.setQuantity(saleQuantity);
+
+        Flux<InventoryDTO> data = Flux.just(inventoryDTO);
+
+        ProductDTO resultProduct = new ProductDTO();
+        resultProduct.setId(id);
+        int quantityAfter = quantityBefore - saleQuantity;
+        resultProduct.setQuantity(quantityAfter);
+        Flux<ProductDTO> result = Flux.just(resultProduct);
+
+        when(saleAddWholesaleUseCase.apply(any())).thenReturn(result);
+
+        webTestClient.post()
+            .uri("/sale/wholesale")
             .contentType(MediaType.APPLICATION_JSON)
             .body(data, InventoryDTO.class)
             .exchange()
